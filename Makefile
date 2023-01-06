5c840b1149394a671c1a0a0db7e38a704922ce91
Russell Brand <brand@responsible.com>
2023-01-06 05:29:12 -0800

    for deploying

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
 start:
 	deno run --allow-net --allow-read main.ts
 
+deploy:
+	git commit -a -m "for deploying"
+	git push
+
+
--- main.ts
+++ main.ts
@@ -1,15 +1,17 @@
-import { Application } from 'https://deno.land/x/abc@v1.0.0-rc10/mod.ts'
+// import { Application } from 'https://deno.land/x/abc@v1.0.0-rc10/mod.ts'
+
+import { Application } from 'https://deno.land/x/abc/mod.ts'
 
 const app = new Application()
 
-// app.static('/', 'public').start({ port: 3002 })
+
 
 
 app.static('/', 'public')
   .file('/', 'public/livepowerfully.xyz/index.html')
   .file('/livepowerfully.xyz', 'public/livepowerfully.xyz/index.html')
-  // .start({ port: 8080 })
-  .start()
+  .start({ port: 8080 })
+  // .start()
 
 
 
